b09b7d254e77e3c08044891fe423bc39dedcffae
Kim Simmons <zoomulator@gmail.com>
2016-12-11 01:41:55 +0100

    trying to get around tuple lengths by injecting closures for the key extraction.

--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,78 @@
+use std::borrow::Borrow;
 
 
 
-
-pub struct JoinIt<ItLeft, ItRight> where
-    ItLeft: Iterator,
-    ItRight: Iterator,
+pub struct JoinIt<I, J, KI, KJ>
 {
-    left: ItLeft,
-    right: ItRight
+    left: I,
+    right: J,
+    key_l: KI,
+    key_r: KJ,
 }
 
 
+
+impl<I,J,KI,KJ,K> Iterator for JoinIt<I,J,KI,KJ> where
+    I: Iterator,
+    J: Iterator,
+    KI: FnMut(I::Item) -> K,
+    KJ: FnMut(J::Item) -> K
+{
+    type Item = (K,K);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(
+            ((self.key_l)(self.left.next().unwrap()),
+             (self.key_r)(self.right.next().unwrap()))
+        )
+    }
+}
+
+/*
+impl<'a, 'b, ItLeft, ItRight, K, V0, V1> Iterator for JoinIt<ItLeft,ItRight> where
+    K: 'a + 'b,
+    V0: 'a,
+    V1: 'b,
+    ItLeft: Iterator<Item=&'a (K, V0)>,
+    ItRight: Iterator<Item=&'b (K, V1)>,
+{
+    type Item = (&'a K, &'a V0, &'b V1);
+
+    next_join!();
+}
+
 impl<ItLeft, ItRight, K, V0, V1> Iterator for JoinIt<ItLeft,ItRight> where
-    ItLeft: Iterator<Item=(K,V0)>,
+    ItLeft: Iterator<Item=(K, V0)>,
     ItRight: Iterator<Item=(K, V1)>,
 {
     type Item = (K, V0, V1);
 
-    fn next(&mut self) -> Option<Self::Item> {
-        let l = self.left.next().unwrap();
-        let r = self.right.next().unwrap();
-        Some((l.0, l.1, r.1))
-    }
+    next_join!();
 }
+*/
+
+
 
 
 trait Joinable
     where Self: Iterator + Sized
 {
-    fn join<I>(self, I) -> JoinIt<Self, I> where
-        I: Iterator;
+    fn join<J,KI,KJ>(self, J, KI, KJ) -> JoinIt<Self,J,KI,KJ> where
+        J: Iterator;
 }
 
 
 impl<I> Joinable for I where
     I: Iterator
 {
-    fn join<J>(self, iter: J)
-        -> JoinIt<Self, J>
-        where J: Iterator {
+    fn join<J,KI,KJ>(self, iter: J, key_l: KI, key_r: KJ)
+        -> JoinIt<I,J,KI,KJ> where
+        J: Iterator {
         JoinIt {
             left: self,
-            right: iter
+            right: iter,
+            key_l: key_l,
+            key_r: key_r
         }
     }
 }
@@ -60,21 +91,23 @@ mod tests {
         let w = vec![66, 77, 88];
         let it2 =  w.iter().enumerate();
 
-        let mut join_it = it.join(it2);
+        let mut join_it = it.join(it2, |(x,_)| x, |(x,_)| x);
         let x = join_it.next();
         println!("{:?}",x);
     }
 
+    /*
     #[test]
     fn it_doesnt_work() {
         let v: Vec<(u32,u32)> = vec![(0,11), (1,22), (2,33)];
         let mut it = v.iter();
 
-        let w = vec![66, 77, 88];
-        let it2 =  w.iter().enumerate();
+        let w: Vec<(u32,u32)> = vec![(0,11), (1,22), (2,33)];
+        let it2 =  w.iter();
 
         let mut join_it = it.join(it2);
         let x = join_it.next();
         println!("{:?}",x);
     }
+    */
 }
