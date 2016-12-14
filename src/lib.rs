bc9c871f393e71907be4b2713393949afef212c9
Kim Simmons <zoomulator@gmail.com>
2016-12-14 20:44:55 +0100

    External iterator seems to be working for value iterators.

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,113 @@
 use std::borrow::Borrow;
+use std::cmp::Ord;
 
 
 
 pub struct JoinIt<I, J, KI, KJ>
 {
-    left: I,
-    right: J,
-    key_l: KI,
-    key_r: KJ,
+    i: I,
+    j: J,
+    ki: KI,
+    kj: KJ,
+}
+
+pub fn join_it<I,J,K,KI,KJ,F>( i: I, j: J, ki: KI, kj: KJ, f: F ) where
+    I: IntoIterator,
+    J: IntoIterator,
+    KI: Fn(&I::Item) -> K,
+    KJ: Fn(&J::Item) -> K,
+    F: Fn(&I::Item, &J::Item),
+    K: Ord
+{
+    use std::cmp::Ordering::*;
+    let mut i = i.into_iter();
+    let mut j = j.into_iter();
+    let mut row = (i.next(), j.next());
+
+    while let (Some(v), Some(w)) = row {
+        match Ord::cmp(&ki(&v), &kj(&w)) {
+            Less => row = (i.next(), Some(w)),
+            Greater => row = (Some(v), j.next()),
+            Equal => {
+                f(&v, &w);
+                row = (i.next(), j.next());
+            },
+        }
+    }
 }
 
 
+impl<'a,I,J,KI,KJ,K> Iterator for JoinIt<I,J,KI,KJ> where
+    I: Iterator,
+    J: Iterator,
+    I::Item: Clone,
+    J::Item: Clone,
+    KI: FnMut(&I::Item) -> K,
+    KJ: FnMut(&J::Item) -> K,
+    K: Ord
+{
+    type Item = (I::Item, J::Item);
 
+    fn next(&mut self) -> Option<Self::Item> {
+        use std::cmp::Ordering::*;
+
+        let mut row = (self.i.next(), self.j.next());
+
+        while let (Some(v), Some(w)) = row {
+            match Ord::cmp(&(self.ki)(&v), &(self.kj)(&w)) {
+                Less => row = (self.i.next(), Some(w)),
+                Greater => row = (Some(v), self.j.next()),
+                Equal => {
+                    return Some((v, w));
+                },
+            }
+        }
+
+        None
+    }
+}
+
+
+/*
 impl<I,J,KI,KJ,K> Iterator for JoinIt<I,J,KI,KJ> where
     I: Iterator,
     J: Iterator,
-    KI: FnMut(I::Item) -> K,
-    KJ: FnMut(J::Item) -> K
+    I::Item: Clone,
+    J::Item: Clone,
+    KI: FnMut(&I::Item) -> K,
+    KJ: FnMut(&J::Item) -> K,
+    K: Ord
 {
-    type Item = (K,K);
+    type Item = (I::Item,J::Item);
 
     fn next(&mut self) -> Option<Self::Item> {
-        Some(
-            ((self.key_l)(self.left.next().unwrap()),
-             (self.key_r)(self.right.next().unwrap()))
-        )
+        use std::cmp::Ordering::*;
+        let mut l = self.left.next();
+        let mut r = self.right.next();
+
+        let rec = |it_l, it_r, key_l, key_r| {
+            let (left,right) = match (it_l.next(), it_r.next()) {
+                (&Some(ref left), &Some(ref right)) => (left, right),
+                _ => return None;
+            }
+
+            match Ord::cmp(&(key_l)(left), &(key_r)(right)) {
+                Less    => { l = self.left.next(); continue; },
+                Greater => { r = self.right.next(); continue; },
+                Equal   => { return Some((left.clone(),right.clone())); }
+            }
+        }
+
+        while let Some((left,right)) = match (&l,&r) {
+                (&Some(ref left), &Some(ref right)) => Some((left, right)),
+                _ => None
+            } {
+        }
+
+        None
     }
 }
+*/
 
 /*
 impl<'a, 'b, ItLeft, ItRight, K, V0, V1> Iterator for JoinIt<ItLeft,ItRight> where
@@ -57,22 +138,26 @@ impl<ItLeft, ItRight, K, V0, V1> Iterator for JoinIt<ItLeft,ItRight> where
 trait Joinable
     where Self: Iterator + Sized
 {
-    fn join<J,KI,KJ>(self, J, KI, KJ) -> JoinIt<Self,J,KI,KJ> where
-        J: Iterator;
+    fn join<J,KI,KJ,K>(self, J, KI, KJ) -> JoinIt<Self,J,KI,KJ> where
+        J: Iterator,
+        KI: FnMut(&Self::Item) -> K,
+        KJ: FnMut(&J::Item) -> K;
 }
 
 
 impl<I> Joinable for I where
     I: Iterator
 {
-    fn join<J,KI,KJ>(self, iter: J, key_l: KI, key_r: KJ)
-        -> JoinIt<I,J,KI,KJ> where
-        J: Iterator {
+    fn join<J,KI,KJ,K>(self, iter: J, ki: KI, kj: KJ) -> JoinIt<I,J,KI,KJ> where
+        J: Iterator,
+        KI: FnMut(&Self::Item) -> K,
+        KJ: FnMut(&J::Item) -> K,
+    {
         JoinIt {
-            left: self,
-            right: iter,
-            key_l: key_l,
-            key_r: key_r
+            i: self,
+            j: iter,
+            ki: ki,
+            kj: kj
         }
     }
 }
@@ -82,6 +167,18 @@ impl<I> Joinable for I where
 mod tests {
     use super::*;
     use Joinable;
+    #[test]
+    fn internal_iterator() {
+        let v = vec!['a', 'b', 'c'];
+        let it = v.iter().enumerate();
+
+        let w = vec![66, 77, 88];
+        let it2 =  w.iter().enumerate();
+
+        join_it( it, it2, |&(x,_)| x, |&(x,_)| x, |&(_,a), &(_,b)| {
+            println!("({:?}, {:?})", a, b);
+        });
+    }
 
     #[test]
     fn move_iterators() {
@@ -91,11 +188,14 @@ mod tests {
         let w = vec![66, 77, 88];
         let it2 =  w.iter().enumerate();
 
-        let mut join_it = it.join(it2, |(x,_)| x, |(x,_)| x);
-        let x = join_it.next();
-        println!("{:?}",x);
+        let mut join_it = it.join(it2, |&(x,_)| x, |&(x,_)| x);
+
+        for x in join_it {
+            println!("{:?}",x);
+        }
     }
 
+    /*
     #[test]
     fn referencing_iterators() {
         let v: Vec<(u32,u32)> = vec![(0,11), (1,22), (2,33)];
@@ -108,4 +208,5 @@ mod tests {
         let x = join_it.next();
         println!("{:?}",x);
     }
+    */
 }
